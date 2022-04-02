a081163f26276241d1e4903b2774f3fdbd0e9783
0x0a0d <vandug23@gmail.com>
2022-04-02 11:21:12 +0700

    remove unused packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,38 @@ module.exports = {
 		"sourceType": "module",
 		"ecmaVersion": "2018"
 	},
+	overrides: [
+		// typescript
+		{
+			files: ["*.ts", "*.tsx"],
+			parser: "@typescript-eslint/parser",
+			plugins: ["@typescript-eslint"],
+			extends: ["plugin:@typescript-eslint/recommended"],
+			rules: {
+				"@typescript-eslint/ban-ts-comment": "off",
+				"@typescript-eslint/no-explicit-any": "off",
+				"@typescript-eslint/type-annotation-spacing": [
+					"warn",
+					{
+						before: false,
+						after: true
+					}
+				]
+			},
+			overrides: [
+				{
+					files: ["*.d.ts"],
+					rules: {
+						"prettier/prettier": "off",
+						"no-use-before-define": "off",
+						"@typescript-eslint/no-unused-vars": "off",
+						"@typescript-eslint/ban-types": "off"
+						// '@typescript-eslint/adjacent-overload-signatures': 'off',
+					}
+				}
+			]
+		}
+	],
 	"rules": {
 		"indent": [
 			"warn",
